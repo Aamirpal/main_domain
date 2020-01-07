13fd0e8a13bae24227a5f29d26ed80a3837a27b0
Aamirpal Sehmi <aamirpal@ucreate.co.in>
2020-01-08 00:19:05 +0530

    link and text changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,18 +81,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a target="_blank" href="https://github.com/Aamirpal">Github</a>
+                    <a target="_blank" href="https://www.twitter.com/aamirpal1">Twitter</a>
                 </div>
             </div>
         </div>
